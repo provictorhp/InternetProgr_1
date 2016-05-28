7955c375dbbdf5de5605a34db816eea84bccad35
Maxim Sadofyev <cfljamtd@gmail.com>
2016-05-28 19:16:36 +0500

    Init

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Save(string text)
+        {
+            string file = Server.MapPath(@"~\App_Data\Database.txt");
+
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                System.IO.File.AppendAllText(file, text + '\n');
+            }
+
+            return View("Index");
+        }
+
+        public ActionResult Show()
+        {
+            string file = Server.MapPath(@"~\App_Data\Database.txt");
+            string[] lines = System.IO.File.ReadAllLines(file);
+            ViewBag.Lines = lines;
+            return View();
+        }
+    }
+}
